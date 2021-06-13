11fb0f25f8c256e718ac7102e6678e78dc2b0ae2
yozakurakirei <matchnokaori@gmail.com>
2021-06-13 10:56:20 +0000

    add: rootからの遷移実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    sessions: 'users/sessions'
-  }
+  devise_for :users
   
   devise_scope :user do
     root to: 'devise/sessions#new'
   end
   
-  get 'index', to: 'pnl#index'
+  get '/mypage', to: 'pnl#index'
   get 'pnl/show'
   get 'pnl/new'
-  get 'pnl/about'
+  get '/about', to: 'pnl#about'
   get 'pnl/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
